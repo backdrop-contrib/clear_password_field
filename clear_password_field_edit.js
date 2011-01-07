a51a3004412ab92ab0f483aa96eb119c3f788fc4
Victor Kareh <vkareh@vkareh.net>
2011-01-07 15:58:59 +0000

    #952848 by kika: Clear both passwords in profile edit form.

--- clear_password_field_edit.js
+++ clear_password_field_edit.js
@@ -3,5 +3,6 @@
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
     $('#edit-pass-pass1').attr('autocomplete', 'OFF').val('');
+    $('#edit-pass-pass2').attr('autocomplete', 'OFF').val('');
   });
 }
